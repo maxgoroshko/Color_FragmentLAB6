cfbec2cf4cc44494da9f89cb4140b3b3c2454a97
MaxGoroshkoTU <tug47632@temple.edu>
2019-10-22 23:01:49 -0400

    canvasActivity

---
+++ app/src/main/java/edu/temple/color_fragment/CanvasActivity.java
@@ -0,0 +1,59 @@
+package edu.temple.color_fragment;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+
+public class CanvasActivity extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "colors";
+
+
+    
+    private String color;
+
+
+    public CanvasActivity() {
+        // Required empty public constructor
+    }
+
+    public static CanvasActivity newInstance(String color) {
+        CanvasActivity fragment = new CanvasActivity();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, color);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            color = getArguments().getString(ARG_PARAM1);
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        LinearLayout view = (LinearLayout) inflater.inflate(R.layout.fragment_canvas, container, false);
+        try {
+            view.setBackgroundColor(Color.parseColor(color));
+        } catch (Exception e){
+            view.setBackgroundColor(Color.WHITE);
+        }
+        return view;
+    }
+}
